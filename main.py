82fe15cea4a52d7e631d9c1b166c34190c1ed5fa
HeavenlyComrade <nikoloudiosjot2@gmail.com>
2023-09-23 18:32:38 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+master_psw = "coolgamer"
+
+password = input("Welcome! Write your password to proceed: ")
+
+if password == master_psw:
+    print("github password: idk")
+    print("youtube password: idk")
+    print("reddit password: idk")
+else: print("Wrong password, try again!")
