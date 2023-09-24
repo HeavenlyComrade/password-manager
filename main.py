a1645e51e3f769a55f16e16afb94b75471808f1a
HeavenlyComrade <nikoloudiosjot2@gmail.com>
2023-09-24 13:03:37 +0300

    Update: Added some kind of encryption(Still working on it) and made the code a bit more efficient

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-master_psw = "coolgamer"
+import hashlib
+master_psw = "coolgamer".encode()
+"SHA-256:", hashlib.sha256(master_psw).hexdigest()
+
 
 password = input("Welcome! Write your password to proceed: ")
 
-if password == master_psw:
-    print("github password: idk")
-    print("youtube password: idk")
-    print("reddit password: idk")
-else: print("Wrong password, try again!")
+def manager():
+    while True:
+     master_psw2 = "coolgamer"
+     if password == master_psw2:
+        print("github password: idk")
+        print("youtube password: idk")
+        print("reddit password: idk")
+        break
+     else: print("Wrong password, try again!")
+     return
+
+manager()
